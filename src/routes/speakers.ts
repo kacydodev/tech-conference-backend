fbf19775cff30eeffca0d9203eb6d6c6cec0ec4d
Kacy Do <contact@kacydo.dev>
2026-06-12 02:52:46 +0700

    modify 'speakers' router to filter featured speaker or get speaker by id

--- src/routes/speakers.ts
+++ src/routes/speakers.ts
@@ -1,15 +1,59 @@
 import {Router} from "express";
-
-interface Env {
-	tech_conference_db: D1Database;
-}
+import {env} from "cloudflare:workers";
 
 const router = Router();
 
-router.get('/', async (_, res) => {
-	const env = process.env as unknown as Env;
-	const speakers = await env.tech_conference_db.prepare('SELECT * FROM speakers').all();
+router.get('/', async (req, res) => {
+	const { isFeatured } = req.query;
+
+	if (typeof isFeatured === 'string' && isFeatured !== 'true') {
+		res.status(400).json({error: 'Invalid query parameter'});
+	}
+
+	const whereClause = isFeatured ? ' WHERE s.featured = 1' : '';
+
+	const query = `
+		SELECT s.id, s.name, s.role, s.avatar, s.company, t.title, tr.color
+		FROM talks t
+			     JOIN speakers s ON t.speaker_id = s.id
+			     INNER JOIN tracks tr ON t.track_id = tr.id
+			${whereClause}
+	`
+	const speakers = await env.tech_conference_db.prepare(query).all();
 	res.json(speakers);
 });
+
+router.get('/:id', async (req,res) => {
+	const { id } = req.params;
+	const query = `
+	SELECT s.id,
+                       s.name,
+                       s.role,
+                       s.company,
+                       s.avatar,
+                       s.bio,
+                       tr.color,
+                       (SELECT json_group_array(
+                                       json_object(
+                                               'id', t.id,
+                                               'title', t.title,
+                                               'description', t.description,
+                                               'location', t.location,
+                                               'day', t.day,
+                                               'start_time', t.start_time,
+                                               'end_time', t.end_time,
+                                               'track', tr.name,
+                                               'color', tr.color
+                                       )
+                               )
+                        FROM talks t
+                                 JOIN tracks tr ON t.track_id = tr.id
+                        WHERE t.speaker_id = s.id) AS talks
+                FROM speakers s JOIN talks t ON s.id = t.speaker_id JOIN tracks tr ON t.track_id = tr.id
+                WHERE s.id = ?
+	`
+	const speaker = await env.tech_conference_db.prepare(query).bind(id).run();
+	res.json(speaker);
+})
 
 export default router;
